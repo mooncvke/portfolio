9caf440a22bf6607d2c24f52958b7453642d833e
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2023-06-02 12:04:20 +0300

    app created and few components added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FrontPage from "./components/FrontPage";
+
+import "./css/App.css";
+
+function App() {
+  return (
+    <div>
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
